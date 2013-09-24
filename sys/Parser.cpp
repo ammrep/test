be384bd27c880be0f80cef159a027f25e8281d03
DefeNder93 <alexdefender93@gmail.com>
2013-09-24 23:27:05 +0400

    Сделал разбиение строки по типам в классе Parser

--- sys/Parser.cpp
+++ sys/Parser.cpp
@@ -40,18 +40,53 @@ void Parser::parse(string input)
 
 	//cout << "Вывод элементов вектора: " << Data[0].str << " " << Data[0].type << " "  << Data[1].str << " " << Data[1].type << " "  << Data[2].str << " " << Data[2].type << endl;
 
-
-	std::string buf("raz dva tri");
-
-
     std::istringstream ist(input);
-    std::string tmp;
+    string tmp;
     while ( ist >> tmp )
 	{
-		cout << tmp << endl;
+		//cout << tmp << endl;
+		if (tmp[0] == '-' && tmp[1] == '-')
+		{
+			d.str = tmp.substr(2,tmp.length());
+			d.type = 'd';	// дополнительный ключ
+			cout << "Тип строки: " << d.type << endl;	// оставил для отладки
+			cout << "Строка: " << d.str << endl;
+		}
+		else
+		if (tmp[0] == '-')
+		{
+			d.str = tmp.substr(1,tmp.length());
+			d.type = 'k';	// ключ
+			cout << "Тип строки: " << d.type << endl;
+			cout << "Строка: " << d.str << endl;
+		}
+		else
+		if (tmp[0] == '"' && tmp[tmp.length() - 1] == '"')
+		{
+			d.str = tmp.substr(1,tmp.length() - 2);
+			d.type = 's';	// строка
+			cout << "Тип строки: " << d.type << endl;
+			cout << "Строка: " << d.str << endl;
+		}
+		else
+		if (tmp[0] == '$')
+		{
+			d.str = tmp.substr(1,tmp.length());
+			d.type = 'v';	// переменная
+			cout << "Тип строки: " << d.type << endl;
+			cout << "Строка: " << d.str << endl;
+		}
+		else
+		{
+			d.str = tmp;
+			d.type = 's';	// строка
+			cout << "Тип строки: " << d.type << endl;
+			cout << "Строка: " << d.str << endl;
+		}
+
 	};
 
-    cout << "Парсим строку: " << input << endl;
+    //cout << "Парсим строку: " << input << endl;
 	if (input == "error")
 		throw AppErrorException();
 	if (input == "echo")
